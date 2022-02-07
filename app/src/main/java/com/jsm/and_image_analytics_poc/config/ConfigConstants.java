f6ed01aa95dba57be42bcdfec5a5ae5f73d0d584
jsainz <jsainz@ayesa.com>
2022-02-07 08:45:03 +0100

    Refactorización de código, adición de capa de datos

--- app/src/main/java/com/jsm/and_image_analytics_poc/config/ConfigConstants.java
+++ app/src/main/java/com/jsm/and_image_analytics_poc/config/ConfigConstants.java
@@ -3,4 +3,5 @@ package com.jsm.and_image_analytics_poc.config;
 public class ConfigConstants {
 
     public static final int REQUEST_CAMERA_PERMISSION = 200;
+    public static final String FRAGMENT_DIALOG = "dialog";
 }
